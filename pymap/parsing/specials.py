747407e8e2cd7e1e2b37a6dcdbadab2a254f859e
Ian Good <icgood@gmail.com>
2014-12-15 21:57:12 -0500

    working "modified utf7" mailbox parsing

--- pymap/parsing/specials.py
+++ pymap/parsing/specials.py
@@ -20,7 +20,6 @@
 #
 
 import re
-import base64
 
 from . import Parseable, NotParseable
 from .primitives import Atom
@@ -47,13 +46,105 @@ class Mailbox(Special):
         self.value = mailbox
 
     @classmethod
+    def _modified_b64encode(cls, src):
+        # Inspired by Twisted Python's implementation:
+        #   https://twistedmatrix.com/trac/browser/trunk/LICENSE
+        src_utf7 = src.encode('utf-7')
+        return src_utf7[1:-1].replace(b'/', b',')
+
+    @classmethod
+    def _modified_b64decode(cls, src):
+        # Inspired by Twisted Python's implementation:
+        #   https://twistedmatrix.com/trac/browser/trunk/LICENSE
+        src_utf7 = b'+' + src.replace(b',', b'/') + b'-'
+        return src_utf7.decode('utf-7')
+
+    @classmethod
+    def encode_name(cls, mailbox):
+        """Encode the mailbox name using the modified UTF-7 specification for
+        IMAP.
+
+        :param str mailbox: The name of the mailbox to encode.
+        :rtype: bytes
+
+        """
+        ret = bytearray()
+        is_usascii = True
+        for i, symbol in enumerate(mailbox):
+            charpoint = ord(symbol)
+            if is_usascii:
+                if charpoint == 0x26:
+                    ret.extend(b'&-')
+                elif charpoint >= 0x20 and charpoint <= 0x7e:
+                    ret.append(charpoint)
+                else:
+                    encode_start = i
+                    is_usascii = False
+            else:
+                if charpoint >= 0x20 and charpoint <= 0x7e:
+                    to_encode = mailbox[encode_start:i]
+                    encoded = cls._modified_b64encode(to_encode)
+                    ret.append(0x26)
+                    ret.extend(encoded)
+                    ret.extend((0x2d, charpoint))
+                    is_usascii = True
+        if not is_usascii:
+            to_encode = mailbox[encode_start:]
+            encoded = cls._modified_b64encode(to_encode)
+            ret.append(0x26)
+            ret.extend(encoded)
+            ret.append(0x2d)
+        return bytes(ret)
+
+    @classmethod
+    def decode_name(cls, encoded_mailbox):
+        """Decode the mailbox name using the modified UTF-7 specification for
+        IMAP.
+
+        :param bytes encoded_mailbox: The encoded name of the mailbox to
+                                      decode.
+        :rtype: str
+
+        """
+        parts = []
+        is_usascii = True
+        buf = memoryview(encoded_mailbox)
+        while buf:
+            byte = buf[0]
+            if is_usascii:
+                if buf[0:2] == b'&-':
+                    parts.append('&')
+                    buf = buf[2:]
+                elif byte == 0x26:
+                    is_usascii = False
+                    buf = buf[1:]
+                else:
+                    parts.append(chr(byte))
+                    buf = buf[1:]
+            else:
+                for i, byte in enumerate(buf):
+                    if byte == 0x2d:
+                        to_decode = buf[:i].tobytes()
+                        decoded = cls._modified_b64decode(to_decode)
+                        parts.append(decoded)
+                        buf = buf[i+1:]
+                        is_usascii = True
+                        break
+        if not is_usascii:
+            to_decode = buf.tobytes()
+            decoded = cls._modified_b64decode(to_decode)
+            parts.append(decoded)
+        return ''.join(parts)
+
+
+    @classmethod
     def parse(cls, buf, **kwargs):
         buf = memoryview(buf)
         atom, buf = Atom.parse(buf)
         mailbox = atom.value
         if mailbox.upper() == b'INBOX':
             return cls('INBOX'), buf
-        return cls(str(mailbox, errors='ignore')), buf
+        return cls(cls.decode_name(mailbox)), buf
 
     def __bytes__(self):
-        return bytes(self.value, 'utf-8')
+        return self.encode_name(self.value)
--- test/test_parsing_specials.py
+++ test/test_parsing_specials.py
@@ -8,10 +8,10 @@ from pymap.parsing.specials import *
 class TestMailbox(unittest.TestCase):
 
     def test_parse(self):
-        ret, buf = Mailbox.parse(b'  \xc9\x91\xc6\xa8\xc6\x8c\xc6\x92  ')
+        ret, buf = Mailbox.parse(b'~peter/mail/&-/&U,BTFw-/&ZeVnLIqe-')
         self.assertIsInstance(ret, Mailbox)
-        self.assertEqual('ɑƨƌƒ', ret.value)
-        self.assertEqual(b'  ', buf)
+        self.assertEqual('~peter/mail/&/台北/日本語', ret.value)
+        self.assertEqual(b'', buf)
 
     def test_parse_inbox(self):
         ret, buf = Mailbox.parse(b'  iNbOx  ')
@@ -24,5 +24,5 @@ class TestMailbox(unittest.TestCase):
             Mailbox.parse(b'  ')
 
     def test_bytes(self):
-        mbx = Mailbox('ɑƨƌƒ')
-        self.assertEqual(b'\xc9\x91\xc6\xa8\xc6\x8c\xc6\x92', bytes(mbx))
+        mbx = Mailbox('~peter/mail/&/台北/日本語')
+        self.assertEqual(b'~peter/mail/&-/&U,BTFw-/&ZeVnLIqe-', bytes(mbx))
