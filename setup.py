bbd52a1f875c3479a8fb0a5476aa9d5f203b4064
Ian Good <icgood@gmail.com>
2018-02-03 12:22:44 -0500

    Set version 0.1.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='pymap',
-      version='0.0.0',
+      version='0.1.0',
       author='Ian Good',
       author_email='icgood@gmail.com',
       description='Lightweight, asynchronous IMAP/POP3 serving in Python.',
