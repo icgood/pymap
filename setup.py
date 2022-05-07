97b261485637d6ec5af5136c5332fac21cc9f276
Ian Good <ian@icgood.net>
2022-05-07 13:15:27 -0400

    Update setup.py
    Reverting the version bump, since `0.27.0` was bumped previously but never released.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('LICENSE.md') as f:
     license = f.read()
 
 setup(name='pymap',
-      version='0.27.1',
+      version='0.27.0',
       author='Ian Good',
       author_email='ian@icgood.net',
       description='Lightweight, asynchronous IMAP serving in Python.',
